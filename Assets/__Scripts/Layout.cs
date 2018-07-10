5fe5ec4f9fc0e8db46295e51ecb86fc8934a9d9b
phone <username2046@gmail.com>
2018-07-10 04:45:20 -0400

    Layed out cards in a tableau

--- Assets/__Scripts/Layout.cs
+++ Assets/__Scripts/Layout.cs
@@ -19,20 +19,23 @@ public class Layout : MonoBehaviour {
     public PT_XMLReader xmlr;
     public PT_XMLHashtable xml;
     public Vector2 multiplier;
+
     public List<SlotDef> slotDefs;
     public SlotDef drawPile;
     public SlotDef discardPile;
 
     public string[] sortingLayerNames = new string[] { "Row0", "Row1", "Row2", "Row3", "Discard", "Draw" };
+
     public void ReadLayout(string xmlText) {
         xmlr = new PT_XMLReader();
         xmlr.Parse(xmlText);
         xml = xmlr.xml["xml"][0];
+
         multiplier.x = float.Parse(xml["multiplier"][0].att("x"));
         multiplier.y = float.Parse(xml["multiplier"][0].att("y"));
 
         SlotDef tSD;
-
+        
         PT_XMLHashList slotsX = xml["slot"];
         for (int i = 0; i < slotsX.Count; i++) {
             tSD = new SlotDef();
--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -1,26 +1,78 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine;
 
 public class Prospector : MonoBehaviour {
 
-	static public Prospector 	S;
-	public Deck					deck;
-    public Layout               layout;
-    public TextAsset			deckXML;
-    public TextAsset            layoutXML;
+    static public Prospector S;
 
-    void Awake(){
-		S = this;
-	}
+    [Header("Set in Inspector")]
+    public TextAsset deckXML;
+    public TextAsset layoutXML;
+    public float xOffset = 3;
+    public float yOffset = -2.5f;
+    public Vector3 layoutCenter;
 
-	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
-		Deck.Shuffle (ref deck.cards);
-        layout = GetComponent<Layout>();
-        layout.ReadLayout(layoutXML.text);
+    [Header("Set Dynamically")]
+    public Deck deck;
+    public Layout layout;
+    public List<CardProspector> drawPile;
+    public Transform layoutAnchor;
+    public CardProspector target;
+    public List<CardProspector> tableau;
+    public List<CardProspector> discardPile;
+
+    void Awake()  {
+        S = this;
     }
 
+    void Start() {
+        deck = GetComponent<Deck>();
+        deck.InitDeck(deckXML.text);
+        Deck.Shuffle(ref deck.cards);
+
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+        LayoutGame();
+    }
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD) {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD) {
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
+        }
+        return (lCP);
+    }
+
+    CardProspector Draw() {
+        CardProspector cd = drawPile[0];
+        drawPile.RemoveAt(0);
+        return (cd);
+    }
+
+    void LayoutGame() {
+        if (layoutAnchor == null) {
+            GameObject tGO = new GameObject("_LayoutAnchor");
+            layoutAnchor = tGO.transform;
+            layoutAnchor.transform.position = layoutCenter;
+        }
+
+        CardProspector cp;
+        foreach (SlotDef tSD in layout.slotDefs) {
+            cp = Draw();
+            cp.faceUp = tSD.faceUp;
+            cp.transform.parent = layoutAnchor;
+            cp.transform.localPosition = new Vector3(
+            layout.multiplier.x * tSD.x,
+            layout.multiplier.y * tSD.y,
+            -tSD.layerID);
+            cp.layoutID = tSD.id;
+            cp.slotDef = tSD;
+            cp.state = eCardState.tableau;
+            tableau.Add(cp);
+        }
+    }
 }
